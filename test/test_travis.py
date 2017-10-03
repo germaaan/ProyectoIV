cee4ab8ab0fcfb8635c8f5cbfffd9e487194ba65
ainokila <universidadcvr@gmail.com>
2017-10-03 09:29:01 +0200

    Aniadido test

--- test/test_travis.py
+++ test/test_travis.py
@@ -1,7 +1,16 @@
-# content of test_sample.py
+import sys
+sys.path.insert(0,'../src')
+from cve import CVE_Details
 
-def inc(x):
-    return x + 1
 
-def test_answer():
-    assert inc(3) == 4
+def test_function():
+    
+    x = CVE_Details()
+    s = json.loads('{"id": "id_cve", "puntuacion": "point", "services": {"vendor": "vendor","product": "producto", "version": "version"}}')
+    x.new(s)
+
+    y = CVE_Details()
+    j = json.loads('{"id": "id_cve", "puntuacion": "point", "services": {"vendor": "vendor","product": "producto", "version": "version"}}')
+    x.newd(j)
+
+    assert comparJson(x,y) == True
