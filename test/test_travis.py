2b182febe125074c15c0fe79ea40dc2d2ff31ca8
ainokila <universidadcvr@gmail.com>
2017-10-03 09:44:51 +0200

    Cambiado a Unitest

--- test/test_travis.py
+++ test/test_travis.py
@@ -1,27 +1,29 @@
-import sys
-sys.path.append(0,'../src')
-from cve import CVE_Details
+import unittest
 
 
-def compare():
+#def compare():
     
-    x = CVE_Details()
-    s = json.loads('{"id": "id_cve", "puntuacion": "point", "services": {"vendor": "vendor","product": "producto", "version": "version"}}')
-    x.new(s)
+    #x = CVE_Details()
+    #s = json.loads('{"id": "id_cve", "puntuacion": "point", "services": {"vendor": "vendor","product": "producto", "version": "version"}}')
+    #x.new(s)
 
-    y = CVE_Details()
-    j = json.loads('{"id": "id_cve", "puntuacion": "point", "services": {"vendor": "vendor","product": "producto", "version": "version"}}')
-    x.newd(j)
+    #y = CVE_Details()
+   # j = json.loads('{"id": "id_cve", "puntuacion": "point", "services": {"vendor": "vendor","product": "producto", "version": "version"}}')
+  #  x.newd(j)
 
-    return comparJson(x,y)
+ #   return comparJson(x,y)
 
 
 
-def test_function():
-    assert compare()
+#def test_function():
+#    assert compare()
 
-def func(x):
-    return x + 1
 
-def test_answer():
-    assert func(3) == 5
+
+
+class TestTravisCVE(unittest.TestCase):
+    def testEjercicio1(self):
+        self.assertEqual(1,1)
+
+if __name__ == '__main__':
+    unittest.main()
