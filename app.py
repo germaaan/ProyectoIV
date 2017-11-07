3528707a60b32bcdc132f0a1d36185da7f38fc34
ainokila <universidadcvr@gmail.com>
2017-11-07 09:45:47 +0100

    Despluegue GET

--- app.py
+++ app.py
@@ -39,10 +39,10 @@ class Insert(Resource):
 class Search(Resource):
     def post(self, servicio,version):
         query = colection.find({'products.product':servicio,'products.version':version})
-        return dumps(query)
+        return json.dumps(query)
     def get(self, servicio,version):
         query = colection.find({'products.product':servicio,'products.version':version})
-        return dumps(query)
+        return json.dumps(query)
 
 
 #Manejador de actualizar la bbdd
