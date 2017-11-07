1baaf1203ca1cba5674af2973083d06a35497b7f
ainokila <universidadcvr@gmail.com>
2017-11-07 10:17:30 +0100

    Despluegue GET

--- app.py
+++ app.py
@@ -40,11 +40,11 @@ class Insert(Resource):
 class Search(Resource):
     def post(self, servicio,version):
         query = colection.find({'products.product':servicio,'products.version':version})
-        print type(dumps(query))
+        print(type(dumps(query)))
         return dumps(query)
     def get(self, servicio,version):
         query = colection.find({'products.product':servicio,'products.version':version})
-        print type(dumps(query))
+        print(type(dumps(query)))
         return dumps(query)
 
 
