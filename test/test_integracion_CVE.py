462e0f7604b4c8319bddbdc3508303bd457a9e01
ainokila <universidadcvr@gmail.com>
2018-01-20 19:27:29 +0100

    Cambio test

--- test/test_integracion_CVE.py
+++ test/test_integracion_CVE.py
@@ -11,11 +11,15 @@ def test_is_up():
 	r = requests.get('http://basecve.westeurope.cloudapp.azure.com/status')
 	return r.status_code == requests.codes.ok
 
+def test_not_valid():
+	r = requests.get('http://basecve.westeurope.cloudapp.azure.com/end-point-not-valid')
+	return r.status_code == 404
 
 class TestTravisCVE(unittest.TestCase):
     def testCVE(self):
         self.assertTrue(test_cve())
 		self.assertTrue(test_is_up())
+		self.assertTrue(test_not_valid())
 
 
 if __name__ == '__main__':
