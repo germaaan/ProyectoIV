92d7b9cbe8dca0a876967afab7ce77135ae781b3
ainokila <universidadcvr@gmail.com>
2018-01-20 19:24:25 +0100

    Cambio test

--- test/test_integracion_CVE.py
+++ test/test_integracion_CVE.py
@@ -7,10 +7,15 @@ def test_cve():
 	r = requests.get('http://basecve.westeurope.cloudapp.azure.com/api/search/internet-explorer/8')
 	return r.status_code == requests.codes.ok
 
+def test_is_up():
+	r = requests.get('http://basecve.westeurope.cloudapp.azure.com/status')
+	return r.status_code == requests.codes.ok
+
 
 class TestTravisCVE(unittest.TestCase):
     def testCVE(self):
         self.assertTrue(test_cve())
+		self.assertTrue(test_is_up())
 
 
 if __name__ == '__main__':
